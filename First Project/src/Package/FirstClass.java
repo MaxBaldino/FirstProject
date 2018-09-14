bd1fe3aa9c44e41f1c924c0a6e5e37174f6687c0
MaxBaldino <43002640+MaxBaldino@users.noreply.github.com>
2018-09-14 08:32:33 -0400

    Everything so far

---
+++ First Project/src/Package/FirstClass.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * Max Baldino
+ * First Submission
+ * 
+ */
+package Package;
+
+public class FirstClass
+{
+
+	public static void main(String[] args) 
+	{
+
+		System.out.print("hello, period 1");
+	}
+
+}
